324c2b9ba5a4758e7f43849aa7f0295cfdf964af
Adam Gerrard <agerrard@email.sc.edu>
2025-03-27 13:57:58 -0400

    fixed it so news articles stay in container, besides the text

--- wine-storage/src/pages/Home.jsx
+++ wine-storage/src/pages/Home.jsx
@@ -12,7 +12,7 @@ function App() {
             description="Welcome to Sip & Store, your ultimate wine storage companion! Our platform allows you to effortlessly manage your personal wine cellar, ensuring that every bottle is cataloged and easily accessible. Add wines to your virtual cellar and visualize their placement with our cellar map. When it's time to enjoy a bottle, move it to your wine history page and add tasting notes to remember every detail of the experience. You can explore reviews and ratings from other wine enthusiasts to discover new favorites. Plus, stay informed with the latest trends, expert insights, and industry news through news articles available right on the homepage. Whether you're a seasoned collector or just starting your journey, Sip & Store makes organizing and enjoying your wine collection simple and enjoyable."/>
         </section>
         <div id = "wines-div" className = "blocks">
-          <section className="columns">
+          <section className="blocks">
             <NewsArticles 
               name="You Should Know the Difference Between Vermouth and Vermouth di Torino"
               image="images/article01.png"/>
