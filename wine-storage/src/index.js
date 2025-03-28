baa46b2a8be6990e23ad4d628cfad076565a1ebe
Adam Gerrard <agerrard@email.sc.edu>
2025-03-27 21:19:54 -0400

    trying to change path to your cellar page

--- wine-storage/src/index.js
+++ wine-storage/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import Layout from './Layout';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
-import About from './pages/About';
+import YourCellar from './pages/YourCellar';
 import Nopage from './pages/Nopage';
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path = "about" element={<About />} />
+          <Route path = "about" element={<YourCellar />} />
           <Route path = "blog" element={<Blog />} />
           <Route path = "*" element={<Nopage />} />
         </Route>
---
+++ wine-storage/src/pages/YourCellar.jsx
@@ -0,0 +1,11 @@
+import './css/YourCellar.css';
+
+function YourCellar() {
+    return (
+        <>
+            <h1>Your Cellar Page</h1>
+        </>
+    );
+}
+
+export default YourCellar;
