3189efa5179e751c7534277e2113ac5fc40536d8
Adam Gerrard <agerrard@email.sc.edu>
2025-03-28 11:58:35 -0400

    consolidated news section

--- wine-storage/src/components/NewsSection.jsx
+++ wine-storage/src/components/NewsSection.jsx
@@ -1,21 +1,11 @@
 import "./css/NewsSection.css";
-import NewsArticles from "./NewsArticles"
 
-function NewsSection() {
+function NewsSection(props) {
     return (
-        <section>
-            <h2 id = "news-articles-header">News Articles</h2>
-            <section className="blocks">
-                <NewsArticles 
-                    name="You Should Know the Difference Between Vermouth and Vermouth di Torino"
-                    image="images/article01.png"/>
-                <NewsArticles 
-                    name="Here's the Trick To Tell if a Riesling Is Going to Be Dry or Sweet"
-                    image="images/article02.png"/>
-                <NewsArticles 
-                    name="Is Older Wine Better Than Younger Wine? Wine Profoessionals Weigh In"
-                    image="images/article03.png"/>
-          </section>
+        <section id = "news-articles" className="news-style">
+            <h3>{props.name}</h3>
+            <p>{props.description}</p>
+            <img id = "image" src={process.env.PUBLIC_URL + "/" + props.image} alt = "news articles"/>
         </section>
     )
 }
--- wine-storage/src/pages/Home.jsx
+++ wine-storage/src/pages/Home.jsx
@@ -12,9 +12,20 @@ function App() {
             image="images/grape-vine.jpg"
             description="Welcome to Sip & Store, your ultimate wine storage companion! Our platform allows you to effortlessly manage your personal wine cellar, ensuring that every bottle is cataloged and easily accessible. Add wines to your virtual cellar and visualize their placement with our cellar map. When it's time to enjoy a bottle, move it to your wine history page and add tasting notes to remember every detail of the experience. You can explore reviews and ratings from other wine enthusiasts to discover new favorites. Plus, stay informed with the latest trends, expert insights, and industry news through news articles available right on the homepage. Whether you're a seasoned collector or just starting your journey, Sip & Store makes organizing and enjoying your wine collection simple and enjoyable."/>
         </section>
-        <section>
-          <NewsSection />
-        </section>
+        <div>
+          <h2 id = "news-articles-header">News Articles</h2>
+          <section className="news-blocks">
+            <NewsSection
+              name="You Should Know the Difference Between Vermouth and Vermouth di Torino"
+              image="images/article01.png"/>
+            <NewsSection
+              name="Here's the Trick To Tell if a Riesling Is Going to Be Dry or Sweet"
+              image="images/article02.png"/>
+            <NewsSection
+              name="Is Older Wine Better Than Younger Wine? Wine Profoessionals Weigh In"
+              image="images/article03.png"/>
+          </section>
+        </div>
         <section>
           <ContactUs />
         </section>
