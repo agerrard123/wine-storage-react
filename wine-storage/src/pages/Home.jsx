907b0c75f4caac7ffb907b307ccd04f7d10f9cb0
Adam Gerrard <agerrard@email.sc.edu>
2025-03-28 20:48:38 -0400

    propperly added grape vine image to home page

--- wine-storage/src/pages/Home.jsx
+++ wine-storage/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import './css/Home.css';
-import HomeDescription from '../components/HomeDescription';
+import GrapeVine from "../images/grape-vine.jpg";
 import NewsArticles from '../components/NewsArticles';
 import ContactUs from '../components/ContactUs';
 
@@ -9,9 +9,8 @@ function App() {
     <>
       <div>
         <section id = "main-content">
-          <HomeDescription
-            image="images/grape-vine.jpg"
-            description="Welcome to Sip & Store, your ultimate wine storage companion! Our platform allows you to effortlessly manage your personal wine cellar, ensuring that every bottle is cataloged and easily accessible. Add wines to your virtual cellar and visualize their placement with our cellar map. When it's time to enjoy a bottle, move it to your wine history page and add tasting notes to remember every detail of the experience. You can explore reviews and ratings from other wine enthusiasts to discover new favorites. Plus, stay informed with the latest trends, expert insights, and industry news through news articles available right on the homepage. Whether you're a seasoned collector or just starting your journey, Sip & Store makes organizing and enjoying your wine collection simple and enjoyable."/>
+          <img id = "image" src = {GrapeVine} alt = "grape vine"/>
+          <p>Welcome to Sip & Store, your ultimate wine storage companion! Our platform allows you to effortlessly manage your personal wine cellar, ensuring that every bottle is cataloged and easily accessible. Add wines to your virtual cellar and visualize their placement with our cellar map. When it's time to enjoy a bottle, move it to your wine history page and add tasting notes to remember every detail of the experience. You can explore reviews and ratings from other wine enthusiasts to discover new favorites. Plus, stay informed with the latest trends, expert insights, and industry news through news articles available right on the homepage. Whether you're a seasoned collector or just starting your journey, Sip & Store makes organizing and enjoying your wine collection simple and enjoyable.</p>
         </section>
         <div>
           <h2 id = "news-articles-header">News Articles</h2>
