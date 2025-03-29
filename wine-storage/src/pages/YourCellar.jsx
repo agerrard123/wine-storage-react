0b058f2bfd9f22d7f87e0a8d5847dde0c9673610
Adam Gerrard <agerrard@email.sc.edu>
2025-03-29 13:04:31 -0400

    added image and button to your cellar and styled it

--- wine-storage/src/pages/YourCellar.jsx
+++ wine-storage/src/pages/YourCellar.jsx
@@ -1,9 +1,19 @@
 import './css/YourCellar.css';
+import Charcuterie from "../images/wine-charcudery.jpg";
 
 function YourCellar() {
     return (
         <>
-            <h1>Your Cellar Page</h1>
+            <div id = "img-div" className = "picture">
+                <section>
+                    <img id = "image" src = {Charcuterie} alt = "wine charcuterie"/>
+                    <div id = "img-div-header">
+                        <h3>Welcome to your wine cellar!</h3>
+                        <p>Add wines that you wish to store in your cellar here!</p>
+                    </div>
+                </section>
+            </div>
+            <div id = "wine-form"><button id="add-wine-form">Use this form to add wine to your cellar</button></div>
         </>
     );
 }
