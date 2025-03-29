0ee108f79af25f2e3421d2f8b814a1e3088778da
Adam Gerrard <agerrard@email.sc.edu>
2025-03-29 15:16:42 -0400

    implemented history01 page and styled it

--- wine-storage/src/pages/History01.jsx
+++ wine-storage/src/pages/History01.jsx
@@ -1,8 +1,17 @@
 import './css/History01.css';
+import hist01pic from "../images/history01.jpg";
 
 function History01() {
     return (
-        <h1>test</h1>
+        <section class = "tasting-notes">
+            <div class = "bottle-img">
+                <img id = "image" src = {hist01pic} alt = "history01 pic"/>
+            </div>
+            <div>
+                <h2>Vernaccia Tasting Notes</h2>
+                <button id = "add-tasting-notes">Click me to add tasting notes!</button>
+            </div>
+        </section>
     )
 }
 
