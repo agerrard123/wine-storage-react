132d2bf0f39ccd0cca71215a7ddf6c46621cb672
Adam Gerrard <agerrard@email.sc.edu>
2025-03-28 20:34:35 -0400

    added image properly to wine history page

--- wine-storage/src/pages/WineHistory.jsx
+++ wine-storage/src/pages/WineHistory.jsx
@@ -1,9 +1,18 @@
 import './css/WineHistory.css';
+import SpilledWine from "../images/spilled-wine.jpg";
 
 function WineHistory() {
     return (
         <>
-        <h1>Wine History Page</h1>
+            <div id = "img-div" class = "picture">
+                <section id = "pic" class = "vineyard">
+                    <img id = "image" src = {SpilledWine} alt = "spilled wine"/>
+                    <div id = "img-div-header">
+                        <h3>Welcome to your wine history!</h3>
+                        <p>Click on each wine to view or add tasting notes from your experience!</p>
+                    </div>
+                </section>
+            </div>
         </>
     )
 };
