a23169f6b60d41bcc08aae0df5770fced0db5952
Adam Gerrard <agerrard@email.sc.edu>
2025-04-01 11:15:56 -0400

    added working toggle nav and styled it

--- wine-storage/src/components/Navigation.jsx
+++ wine-storage/src/components/Navigation.jsx
@@ -1,15 +1,22 @@
 import {Outlet, Link} from "react-router-dom";
+import {useState} from "react";
 import './css/Navigation.css';
 
 function Navigation() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
     return (
         <nav id="main-nav">
-            <div id = "toggle-nav">
+            <div id = "toggle-nav" onClick = {toggleMenu}>
                 <div></div>
                 <div></div>
                 <div></div>
             </div>
-            <ul>
+            <ul className = {menuOpen ? "" : "hide-small"}>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/cellar">Your Cellar</Link></li>
                 <li><Link to="/map">Map</Link></li>
