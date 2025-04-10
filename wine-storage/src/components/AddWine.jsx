79da74bef44018b5835a10e564cdb4625a4779b0
Adam Gerrard <agerrard@email.sc.edu>
2025-04-10 11:54:46 -0400

    styling dialog

--- wine-storage/src/components/AddWine.jsx
+++ wine-storage/src/components/AddWine.jsx
@@ -37,69 +37,53 @@ const AddWine = (props) => {
             <div className="w3-modal-content">
                 <div className="w3-container">
                     <span id="dialog-close" className="w3-button w3-display-topright" onClick = {props.closeAddDialog}>&times;</span>
+                    <h2 id = "form-dialog-head">Add a New Wine to Your Cellar</h2>
                     <form id="add-property-form" onSubmit = {addToServer}>
-                        <h3>Add a New Wine to Your Cellar</h3>
-
-                        <p>
-                            <label htmlFor="winery">Winery: </label>
-                            <input type="text" id="winery" name="winery" required min="3"></input>
-                        </p>
-
-                        <p>
-                            <label htmlFor="vintage">Vintage: </label>
-                            <input type="number" id="vintage" name="vintage" min="1900" required></input>
-                        </p>
-
-                        <p>
-                            <label htmlFor="price">Price: </label>
-                            <input type="number" id="price" name="price" min="0" required></input>
-                        </p>
-
-                        <p>
-                            <label htmlFor="country">Country: </label>
-                            <input type="text" id="country" name="country" min="3" required></input>
-                        </p>
-
-                        <p>
-                            <label htmlFor="region">Region: </label>
-                            <input type="text" id="region" name="region" min="3" required></input>
-                        </p>
-
-                        <p>
-                            <label htmlFor="description">Description: </label>
-                            <input type="text" id="description" name="description" min="3" required></input>
-                        </p>
-
-                        <p>
-                            <label htmlFor="grape">Grape: </label>
-                            <input type="text" id="grape" name="grape" min="3" required></input>
-                        </p>
-
-                        <p>
-                            <label htmlFor="cellarLocation">Cellar Location: </label>
-                            <input type="text" id="cellarLocation" name="cellarLocation" min="2" required></input>
-                        </p>
-
-                        <section className="columns">
-                            <div>
-                                <p id="img-prev-section">
-                                    {prevSrc!==""?
-                                    (<img id = "img-prev" src={prevSrc} alt= "new pic"></img>):
-                                    ("")
-                                    } 
+                        <section id = "columns">
+                            <div id = "form-container">
+                                <p id = "form-p-1">
+                                    <label htmlFor="winery">Winery: </label> <br/> <br/>
+                                    <label htmlFor="vintage">Vintage: </label> <br/> <br/>
+                                    <label htmlFor="price">Price: $</label> <br/> <br/>
+                                    <label htmlFor="country">Country: </label> <br/> <br/>
+                                    <label htmlFor="region">Region: </label> <br/> <br/>
+                                    <label htmlFor="description">Description: </label> <br/> <br/>
+                                    <label htmlFor="grape">Grape: </label> <br/> <br/>
+                                    <label htmlFor="cellarLocation">Cellar Location: </label> <br/> <br/>
                                 </p>
-                            </div> 
-                            <p id="img-upload">
-                                <label htmlFor="img">Upload Image: </label>
-                                <input type="file" id="img" name="img" accept="image/*" onChange={uploadImage}/>
-                            </p>
+                                <p id = "form-p-2">
+                                    <input type="text" id="winery" name="winery" required min="3"></input> <br/> <br/>
+                                    <input type="number" id="vintage" name="vintage" min="1900" required></input> <br/> <br/>
+                                    <input type="number" id="price" name="price" min="0" required></input> <br/> <br/>
+                                    <input type="text" id="country" name="country" min="3" required></input> <br/> <br/>
+                                    <input type="text" id="region" name="region" min="3" required></input> <br/> <br/>
+                                    <input type="text" id="description" name="description" min="3" required></input> <br/> <br/>
+                                    <input type="text" id="grape" name="grape" min="3" required></input> <br/> <br/>
+                                    <input type="text" id="cellarLocation" name="cellarLocation" min="2" required></input> <br/> <br/>
+                                </p>
+                                
+                            </div>
+                            <section className="upload-image">
+                                <p id="img-upload">
+                                    <label htmlFor="img">Upload Image: </label>
+                                    <input type="file" id="img" name="img" accept="image/*" onChange={uploadImage}/>
+                                </p>
+                                <div>
+                                    <p id="img-prev-section">
+                                        {prevSrc!==""?
+                                        (<img id = "img-prev" src={prevSrc} alt= "new pic"></img>):
+                                        ("")
+                                        } 
+                                    </p>
+                                </div> 
+                            </section>
+                            <p>{result}</p>
                         </section>
-
-                        <p>
+                        <p id = "submit">
                             <button type="submit">Submit</button>
                         </p>
-                        <p>{result}</p>
                     </form>
+                    
                 </div>
             </div>
         </div>
